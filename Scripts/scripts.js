979c58ac1427de28e4d9f7314f980f0b4b62a00f
sqlt <etakota@goldmail.etsu.edu>
2017-04-04 18:48:26 -0400

    initial commit

---
+++ Scripts/scripts.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular.module("repos", [])
+                .controller("reposController", function ($scope, $http, $log) {
+                    $scope.inputValue = null;
+                    $scope.getUser = function () {
+
+                        var successCallBack = function (response) {
+                            $scope.repos = response.data;
+                        };
+
+                        var errorCallBack = function (response) {
+                            $scope.error = response.data;
+                        };
+
+                        $http({
+                            method: 'GET',
+                            url: 'https://api.github.com/users/' + $scope.inputValue + '/repos'
+                        })
+                        .then(successCallBack, errorCallBack);
+                    };
+                });
